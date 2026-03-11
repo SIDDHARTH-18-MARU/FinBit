c96f3892b2ac05c35e90515ee37e18025b147100
SIDDHARTH-18-MARU <siddharthmaru198@gmail.com>
2026-03-11 17:54:46 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  async rewrites() {
+    const target = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8080";
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${target}/api/:path*`,
+      },
+    ];
+  },
+}
+
+export default nextConfig
